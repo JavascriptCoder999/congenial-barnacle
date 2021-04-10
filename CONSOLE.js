f353dfb93ebabcb57ff79945616fe35ec6d70350
Lucas Peel <81769195+JavascriptCoder999@users.noreply.github.com>
2021-04-10 15:34:39 +0100

    Update and rename ELEMENT.js to CONSOLE.js

---
+++ CONSOLE.js
@@ -0,0 +1,10 @@
+//console message
+console.log("my online readme");
+console.log("my online readme");
+console.log("my online readme");
+//sends message on console three times
+console.log("👋 Hi, I’m @JavascriptCoder999");
+console.log("👀 I’m interested in coding scratch, html and javascript"); 
+console.log("🌱 I’m currently learning shell");
+console.log("💞️ I’m looking to collaborate on game projects and other cool stuff");
+console.log("📫 How to reach me - email lucaspeel93@outlook.com or skype: Lucas Peel");
