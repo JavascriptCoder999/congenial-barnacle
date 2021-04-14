4d991fa00d832af14185f6c2618ece01bb55acad
Lucas Peel <81769195+JavascriptCoder999@users.noreply.github.com>
2021-04-14 15:10:13 +0100

    Update csharp.cs

--- csharp.cs
+++ csharp.cs
@@ -8,3 +8,45 @@ while(test_1 = true)
 {
 Console.WriteLine("my test repository");
 }
+using System;
+
+namespace MyApplication
+{
+  class Animal  // Base class (parent) 
+  {
+    public virtual void animalSound()
+    {
+      Console.WriteLine("This is a cool repo");
+    }
+  }
+
+  class Pig : Animal  // Derived class (child) 
+  {
+    public override void animalSound()
+    {
+      Console.WriteLine("of c# coding");
+    }
+  }
+
+  class Dog : Animal  // Derived class (child) 
+  {
+    public override void animalSound()
+    {
+      Console.WriteLine("I HOPE YOU LIKE IT PLEASE STAR YAY.");
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Animal myAnimal = new Animal();
+      Animal myPig = new Pig();
+      Animal myDog = new Dog();  // exec.ute
+      
+      myAnimal.animalSound();
+      myPig.animalSound();
+      myDog.animalSound();
+    }
+  }
+}
